554c29a8d3d63537451ffbf9fb39ff3683320fd4
Animesh Kahara <animeshcyberphysco@gmail.com>
2020-12-07 22:33:22 +0530

    Initial Commmit

---
+++ main.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <string.h>
+
+using namespace std;
+
+struct Array{
+    int A[10];
+    int length;
+    int szie;
+};
+
+
+int main()
+{
+
+    struct Array arr = {{2,4,6,7,8},5,10};
+    return 0;
+}
+
+
+
+
+
