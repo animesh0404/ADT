d06e0a3a06c03382bcdba642567dc520d0851c2b
Animesh Kahara <animeshcyberphysco@gmail.com>
2020-12-07 23:16:16 +0530

    -Added Linear Search functions and Code Refactoring.

--- main.cpp
+++ main.cpp
@@ -1,6 +1,7 @@
 /*---------------------------
 //Author:     Animesh Kahara
 //Program:    Array ADT
+//All rights reserved, Copyright 2020
 */
 
 #include <iostream>
@@ -8,12 +9,53 @@
 
 using namespace std;
 
+//Declrations
 struct Array{
     int A[10];
     int length;
     int szie;
 };
 
+void intSwap(int *,int *);
+void Display(struct Array);
+void Append(struct Array *,int);
+void Insert(struct Array *,int,int);
+int  Delete(struct Array *,int);
+int  LinearSearch(struct Array,int);
+int  LinearSearchv1(struct Array *,int);
+int  LinearSearchv2(struct Array *,int);
+
+
+//Driver Code
+
+int main()
+{
+    struct Array arr = {{2,4,6,7,8},5,10};
+
+    printf("\nAppending 34 at the end");
+    Append(&arr,34);
+    printf("\nAfter Appending");
+    Display(arr);
+
+    printf("\nInserting 21 at index 2");
+    Insert(&arr,2,21);
+    Display(arr);
+
+    printf("\nDeleting key at index 2");
+    printf("\nElement deleted: %d",Delete(&arr,2));
+    printf("\nAfter Deletion:");
+    Display(arr);
+
+    printf("\nSearching for key: 6");
+    printf("\nFound at index: %d",LinearSearch(arr,6));
+    printf("\nAfter Searching:");
+    Display(arr);
+
+    return 0;
+}
+
+
+//All Functions
 
 //Function for traversing/Displaying the complete array.
 void Display(struct Array a)
@@ -66,30 +108,57 @@ int Delete(struct Array *arr,int index)
 }
 
 
-int main()
+//Function's to perform Searching in Linear Time
+int LinearSearch(struct Array arr,int key)
 {
-    struct Array arr = {{2,4,6,7,8},5,10};
+    int i;
+    for(i=0; i<arr.length; i++)
+    {
+        if(key==arr.A[i])
+            return i;
+    }
+    return -1;
+}
 
-    printf("\nAppending 34 at the end");
-    Append(&arr,34);
-    printf("\nAfter Appending");
-    Display(arr);
+int LinearSearchv1(struct Array *arr,int key)
+{
+    //Improvement in search using Transposition
+    //Gradually increases/decreases search time based on
+    //frequency of the searched key.
 
-    printf("\nInserting 21 at index 2");
-    Insert(&arr,2,21);
-    Display(arr);
+    int i;
+    for(i=0; i<arr->length; i++)
+    {
+        if(key==arr->A[i])
+        {
+         intSwap(&arr->A[i], &arr->A[i-1]);
+         return i-1;
+        }
+    }
+    return -1;
+}
 
-    printf("\nDeleting key at index 2");
-    Delete(&arr,2);
-    printf("\nAfter Deletion:");
-    Display(arr);
-
-
-
-    return 0;
+int LinearSearchv2(struct Array *arr,int key)
+{
+    //Improvement in search using Move to Head/Front
+    //Constant time taken to return last searched key
+    int i;
+    for(i=0; i<arr->length; i++)
+    {
+        if(key==arr->A[i])
+        {
+         intSwap(&arr->A[i], &arr->A[0]);
+         return 0;
+        }
+    }
+    return -1;
 }
 
 
-
-
-
+//Function to swap any two integer values.
+void intSwap(int *A,int *B)
+{
+    int temp = *A;
+    *A = *B;
+    *B = temp;
+}
