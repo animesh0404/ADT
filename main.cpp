b410d55d0e38db940536de37176177a6611b7202
Animesh Kahara <animeshcyberphysco@gmail.com>
2020-12-07 22:43:07 +0530

    Added Display function

--- main.cpp
+++ main.cpp
@@ -9,11 +9,20 @@ struct Array{
     int szie;
 };
 
+void Display(struct Array a)
+{
+    int i;
+    for(i=0;i<a.length;i++)
+        printf("\n%d",a.A[i]);
+}
+
 
 int main()
 {
-
     struct Array arr = {{2,4,6,7,8},5,10};
+
+    Display(arr);
+
     return 0;
 }
 
