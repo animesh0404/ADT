4f5336fdc5607a51407ce4a4be2020f5a7bcf1f6
animesh0404 <animeshcyberphysco@gmail.com>
2021-01-05 21:57:11 +0530

    -Changed main driver code and created temporary String_ADT.

--- main.cpp
+++ main.cpp
@@ -4,83 +4,146 @@
 //All rights reserved, Copyright 2020
 */
 
-#include<iostream>
+#include<stdio.h>
 #include<stdlib.h>
-#include "ArrayADT.h"
 
-using namespace std;
+struct String {
+       char *A;
+       int length;
+       int szie;
+};
 
-//Driver Code
+//using namespace std;
+int getLength(struct String);
+void initString(struct String *);
+void initString(struct String *,int);
+void setString(struct String *, char[],int);
+void getString(struct String);
+
 int main()
 {
-    char ch;
-    int ele;
-    int x,index;
+    struct String name,msg;
+    char *temp;
+    initString(&name);
+    initString(&msg);
 
-    cout<<"\nEnter size of array: ";
-    cin>>ele;
+    setString(&name, "Animesh",7);
 
-    Array arr(ele);
+//    printf("\n Please enter your meassage: ");
+//    scanf("%s",&temp);
+//    setString(&name,temp);
 
-    do
-    {
-        cout<<"\n\nMenu\n";
-        cout<<"1. Insert\n";
-        cout<<"2. Delete\n";
-        cout<<"3. Search\n";
-        cout<<"4. Sum\n";
-        cout<<"5. Display\n";
-        cout<<"6. Average\n";
-        cout<<"7. Exit\n";
+    printf("\n\nLength: %d",getLength(name));
 
-        cout<<"enter you choice: ";
-        cin>>ch;
+    getString(name);
+//    getString(msg);
 
-
-        switch(ch)
-        {
-         case '1':
-            cout<<"\nEnter (index,value): ";
-            cin>>index>>x;
-            arr.Insert(index,x);
-            break;
-
-         case '2':
-            cout<<"Enter index ";
-            cin>>index;
-            x=arr.Delete(index);
-            cout<<"Deleted Element is "<<x<<endl;
-            break;
-
-         case '3':
-            cout<<"Enter element to search ";
-            cin>>x;
-            index=arr.LinearSearch(x);
-            cout<<"Element index "<<index<<endl;
-            break;
-
-         case '4':
-             cout<<"Sum is "<<arr.Sum()<<endl;
-            break;
-
-         case '5':
-            arr.Display();
-            break;
-
-         case '6':
-            cout<<"Avg is: "<<arr.Avg()<<endl;
-            break;
-
-         case '7':
-            break;
-
-         default:
-            cout<<"\n\n\t\tWRONG INPUT";
-            cin>>ele;
-        }
-    }while(ch!='7');
+//    getString(name);
 
     return 0;
 }
 
+void initString(struct String *a)
+{
+    a->A = "";
+    a->length = 0;
+    a->szie = 10;
+}
 
+void initString(struct String *a,int s)
+{
+    a->A = (char *)malloc(s * sizeof(struct String));
+    a->length = 0;
+    a->szie = s;
+}
+
+
+int getLength(struct String n)
+{
+ return n.length;
+}
+
+void setString(struct String *a,char n[],int l)
+{
+    a->A = n;
+    a->length = l;
+}
+
+void getString(struct String a)
+{
+    printf("\n%s", a.A);
+}
+////Driver Code
+//int main()
+//{
+//    char ch;
+//    int ele;
+//    int x,index;
+//
+//    cout<<"\nEnter size of array: ";
+//    cin>>ele;
+//
+//    Array arr(ele);
+//
+//    do
+//    {
+//        cout<<"\n\nMenu\n";
+//        cout<<"1. Insert\n";
+//        cout<<"2. Delete\n";
+//        cout<<"3. Search\n";
+//        cout<<"4. Sum\n";
+//        cout<<"5. Display\n";
+//        cout<<"6. Average\n";
+//        cout<<"7. Exit\n";
+//
+//        cout<<"enter you choice: ";
+//        cin>>ch;
+//
+//
+//        switch(ch)
+//        {
+//         case '1':
+//            cout<<"\nEnter (index,value): ";
+//            cin>>index>>x;
+//            arr.Insert(index,x);
+//            break;
+//
+//         case '2':
+//            cout<<"Enter index ";
+//            cin>>index;
+//            x=arr.Delete(index);
+//            cout<<"Deleted Element is "<<x<<endl;
+//            break;
+//
+//         case '3':
+//            cout<<"Enter element to search ";
+//            cin>>x;
+//            index=arr.LinearSearch(x);
+//            cout<<"Element index "<<index<<endl;
+//            break;
+//
+//         case '4':
+//             cout<<"Sum is "<<arr.Sum()<<endl;
+//            break;
+//
+//         case '5':
+//            arr.Display();
+//            break;
+//
+//         case '6':
+//            cout<<"Avg is: "<<arr.Avg()<<endl;
+//            break;
+//
+//         case '7':
+//            break;
+//
+//         default:
+//            cout<<"\n\n\t\tWRONG INPUT";
+//            cin>>ele;
+//        }
+//    }while(ch!='7');
+//
+//    return 0;
+//}
+//
+//
